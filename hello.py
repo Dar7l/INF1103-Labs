48ee5a098d89fcd381bc622782d38ba5fe0314cf
Daryl <darylng12@gmail.com>
2026-09-04 11:23:59 +0800

    Activity 2

--- hello.py
+++ hello.py
@@ -2,3 +2,13 @@ print("======================")
 print("Welcome here")
 print("My first post!")
 print("======================")
+
+
+username = "Daryl"
+bio = "Fintech student"
+followers = 200
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
